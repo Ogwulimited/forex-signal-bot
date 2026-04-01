e99f8ea25d52d52c8cdaa326305457153f4f63b4
Ogwulimited <ogwufrancis2002@gmail.com>
2026-04-01 01:02:59 +0100

    Updated signal_dispatcher.py

--- signal_dispatcher.py
+++ signal_dispatcher.py
@@ -1,6 +1,2 @@
-from signal_formatter import format_new_signal
-from telegram_sender import send_telegram_message
-
-def dispatch_new_signal(trade, pair, timeframe):
-    message = format_new_signal(trade, pair, timeframe)
-    send_telegram_message(message)
+def generate_signal(bias):
+    return None

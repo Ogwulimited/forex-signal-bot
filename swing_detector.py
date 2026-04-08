6851987b4148ce862ede2a3ac98a28f48dd557f3
Ogwulimited <ogwufrancis2002@gmail.com>
2026-04-08 10:07:06 +0100

    Update swing_detector.py

--- swing_detector.py
+++ swing_detector.py
@@ -1,29 +1,77 @@
-def find_swings(candles, left=2, right=2):
-    swing_highs = []
-    swing_lows = []
+"""
+Swing high and swing low detector.
+Identifies local highs/lows using left/right candle comparison.
+"""
 
-    for i in range(left, len(candles) - right):
-        current_high = candles[i]["high"]
-        current_low = candles[i]["low"]
-
-        left_highs = [candles[j]["high"] for j in range(i - left, i)]
-        right_highs = [candles[j]["high"] for j in range(i + 1, i + 1 + right)]
-
-        left_lows = [candles[j]["low"] for j in range(i - left, i)]
-        right_lows = [candles[j]["low"] for j in range(i + 1, i + 1 + right)]
-
-        if current_high > max(left_highs) and current_high > max(right_highs):
-            swing_highs.append({
-                "index": i,
-                "price": current_high,
-                "candle": candles[i]
+def find_swing_highs(candles, left=2, right=2):
+    """
+    Find all swing highs in the candle list.
+    
+    Parameters:
+    - candles: list of dicts with 'high', 'low', 'close'
+    - left: number of candles to look left
+    - right: number of candles to look right
+    
+    Returns:
+    - list of dicts with keys: index, level (high price)
+    """
+    swings = []
+    n = len(candles)
+    
+    for i in range(left, n - right):
+        is_swing = True
+        # Check left candles
+        for j in range(1, left + 1):
+            if candles[i]['high'] <= candles[i - j]['high']:
+                is_swing = False
+                break
+        # Check right candles
+        if is_swing:
+            for j in range(1, right + 1):
+                if candles[i]['high'] <= candles[i + j]['high']:
+                    is_swing = False
+                    break
+        if is_swing:
+            swings.append({
+                'index': i,
+                'level': candles[i]['high']
             })
+    
+    return swings
 
-        if current_low < min(left_lows) and current_low < min(right_lows):
-            swing_lows.append({
-                "index": i,
-                "price": current_low,
-                "candle": candles[i]
+
+def find_swing_lows(candles, left=2, right=2):
+    """
+    Find all swing lows in the candle list.
+    
+    Parameters:
+    - candles: list of dicts with 'high', 'low', 'close'
+    - left: number of candles to look left
+    - right: number of candles to look right
+    
+    Returns:
+    - list of dicts with keys: index, level (low price)
+    """
+    swings = []
+    n = len(candles)
+    
+    for i in range(left, n - right):
+        is_swing = True
+        # Check left candles
+        for j in range(1, left + 1):
+            if candles[i]['low'] >= candles[i - j]['low']:
+                is_swing = False
+                break
+        # Check right candles
+        if is_swing:
+            for j in range(1, right + 1):
+                if candles[i]['low'] >= candles[i + j]['low']:
+                    is_swing = False
+                    break
+        if is_swing:
+            swings.append({
+                'index': i,
+                'level': candles[i]['low']
             })
-
-    return swing_highs, swing_lows
+    
+    return swings

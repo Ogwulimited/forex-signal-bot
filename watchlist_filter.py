6d109c31cc89e496b5f9bcf8cfaf0882dbd3d4d8
Ogwulimited <ogwufrancis2002@gmail.com>
2026-04-20 14:33:39 +0100

    Update watchlist_filter.py

--- watchlist_filter.py
+++ watchlist_filter.py
@@ -1,17 +1,33 @@
-def is_strong_watchlist_candidate(bias):
-    if not bias.get("aligned"):
-        return False
+"""
+HTF Watchlist Filter
+Determines if a pair passes the higher-timeframe criteria.
+"""
 
-    if bias["strength_4h"] < 3:
+def passes_watchlist(bias_data):
+    """
+    Check if a pair should be on the HTF watchlist.
+    
+    Parameters:
+    - bias_data: dict from mtf_bias_engine.get_mtf_bias()
+    
+    Returns:
+    - True if all criteria met, else False
+    """
+    if not bias_data.get('aligned', False):
         return False
-
-    if bias["strength_1h"] < 3:
+    
+    # Relaxed thresholds for quantity over quality
+    STRENGTH_MIN = 2          # Was 3
+    RANGE_RATIO_4H_MIN = 0.002   # Was 0.003
+    RANGE_RATIO_1H_MIN = 0.001   # Was 0.0015
+    
+    if bias_data['strength_4h'] < STRENGTH_MIN:
         return False
-
-    if bias["range_ratio_4h"] < 0.003:
+    if bias_data['strength_1h'] < STRENGTH_MIN:
         return False
-
-    if bias["range_ratio_1h"] < 0.0015:
+    if bias_data['range_ratio_4h'] < RANGE_RATIO_4H_MIN:
         return False
-
+    if bias_data['range_ratio_1h'] < RANGE_RATIO_1H_MIN:
+        return False
+    
     return True

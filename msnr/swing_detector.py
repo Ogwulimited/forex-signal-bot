19ffce2a0ebb3b554a14686df51fb3fefa932ec8
Ogwulimited <ogwufrancis2002@gmail.com>
2026-09-25 17:45:54 +0100

    Add swing high and low detection functions
    This module provides functions to detect swing highs and lows in a list of candles, allowing for technical analysis in trading strategies.

---
+++ msnr/swing_detector.py
@@ -0,0 +1,74 @@
+"""
+Swing high and swing low detector.
+Identifies local highs/lows using left/right candle comparison.
+Copied into msnr/ to keep this strategy self-contained.
+"""
+
+def find_swing_highs(candles, left=2, right=2):
+    """
+    Find all swing highs in the candle list.
+
+    Parameters:
+    - candles: list of dicts with 'high', 'low', 'close'
+    - left: number of candles to look left
+    - right: number of candles to look right
+
+    Returns:
+    - list of dicts with keys: index, level (high price)
+    """
+    swings = []
+    n = len(candles)
+
+    for i in range(left, n - right):
+        is_swing = True
+        for j in range(1, left + 1):
+            if candles[i]['high'] <= candles[i - j]['high']:
+                is_swing = False
+                break
+        if is_swing:
+            for j in range(1, right + 1):
+                if candles[i]['high'] <= candles[i + j]['high']:
+                    is_swing = False
+                    break
+        if is_swing:
+            swings.append({
+                'index': i,
+                'level': candles[i]['high']
+            })
+
+    return swings
+
+
+def find_swing_lows(candles, left=2, right=2):
+    """
+    Find all swing lows in the candle list.
+
+    Parameters:
+    - candles: list of dicts with 'high', 'low', 'close'
+    - left: number of candles to look left
+    - right: number of candles to look right
+
+    Returns:
+    - list of dicts with keys: index, level (low price)
+    """
+    swings = []
+    n = len(candles)
+
+    for i in range(left, n - right):
+        is_swing = True
+        for j in range(1, left + 1):
+            if candles[i]['low'] >= candles[i - j]['low']:
+                is_swing = False
+                break
+        if is_swing:
+            for j in range(1, right + 1):
+                if candles[i]['low'] >= candles[i + j]['low']:
+                    is_swing = False
+                    break
+        if is_swing:
+            swings.append({
+                'index': i,
+                'level': candles[i]['low']
+            })
+
+    return swings
